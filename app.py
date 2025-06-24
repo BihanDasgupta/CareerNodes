3cd921d3c10b3981b0a1f9b6b78adb733ac64ae4
Bihan Dasgupta <dasguptb@uci.edu>
2025-06-24 10:42:53 -0400

    fixing graph??

--- app.py
+++ app.py
@@ -448,22 +448,22 @@ if st.button("Find Matches"):
  
         st.markdown('<hr>', unsafe_allow_html=True)
 
-    # Create and display the network graph
-    st.subheader("🕸️ Career Network Visualization")
-    st.markdown('<div class="graph-container">', unsafe_allow_html=True)
-    
-    G = nx.Graph()
-    G.add_node("You")
-    for score, internship, _ in results:
-        node = f"{internship['company']}\n{internship['title']}"
-        G.add_node(node)
-        G.add_edge("You", node, weight=score)
-    net = Network(height="600px", width="100%", bgcolor="#222222", font_color="white")
-    net.from_nx(G)
-    net.save_graph("graph.html")
+         # Create and display the network graph
+        st.subheader("🕸️ Career Network Visualization")
+        st.markdown('<div class="graph-container">', unsafe_allow_html=True)
+        
+        G = nx.Graph()
+        G.add_node("You")
+        for score, internship, _ in results:
+            node = f"{internship['company']}\n{internship['title']}"
+            G.add_node(node)
+            G.add_edge("You", node, weight=score)
+        net = Network(height="600px", width="100%", bgcolor="#222222", font_color="white")
+        net.from_nx(G)
+        net.save_graph("graph.html")
 
-    with open("graph.html", "r", encoding='utf-8') as HtmlFile:
-        source_code = HtmlFile.read()
-        components.html(source_code, height=650, width=900)
-    
-    st.markdown('</div>', unsafe_allow_html=True)
+        with open("graph.html", "r", encoding='utf-8') as HtmlFile:
+            source_code = HtmlFile.read()
+            components.html(source_code, height=650, width=900)
+        
+        st.markdown('</div>', unsafe_allow_html=True)
